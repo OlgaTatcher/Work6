bac1d9672dce8426d55c42758464cb6f95e54131
OlgaTatcher <dai.akemi230318@gmail.com>
2023-09-21 15:26:59 +0300

    added variables, method <isPalindrome> in class <Palindrome>

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,6 +1,33 @@
 public class Palindrome {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        Palindrome palindrome =new Palindrome();
+        String firstStringInput="A man, a plan, a canal: Panama";
+        String secondStringInput="race a car";
+        String thirdStringInput=" ";
+        boolean firstResult= palindrome.isPalindrome(firstStringInput);
+        boolean secondResult= palindrome.isPalindrome(secondStringInput);
+        boolean thirdResult= palindrome.isPalindrome(thirdStringInput);
+        System.out.println(firstResult);
+        System.out.println(secondResult);
+        System.out.println(thirdResult);
     }
+    public boolean isPalindrome(String startLine){
+
+        if (startLine.isEmpty()){
+            return true;
+        }
+        startLine=startLine.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+
+        int leftChar=0;
+        int rightChar=startLine.length()-1;
+        while (leftChar<rightChar){
+            if(startLine.charAt(leftChar)!=startLine.charAt(rightChar)){
+                return false;
+            }
+            leftChar++;
+            rightChar--;
+        }
+        return true;
+    }
+
 }
